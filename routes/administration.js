67b1be8cb4da1c8423a2e3df0974372ceeb8ed21
jgiere <jogiere@gmail.com>
2014-10-29 21:03:48 +1300

    some tiny code changes

--- routes/administration.js
+++ routes/administration.js
@@ -3,26 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-	pg.connect(conString, function(err, client, done) {
-		if(err) {
-			return console.error('error fetching client from pool', err);
-		}
-		client.query('SELECT * FROM "' + nconf.get('database').name + '".customer', function(err, result) {
-				done();
-				
-				if(err) {
-					return console.error('error running query', err);
-				}
-				var customerArr = []
-				for(var i = 0; i < result.rows.length; i++) {
-					customerArr[i] = { "id": result.rows[i].id, "name": result.rows[i].name };
-				}
-				
-				client.end();
-				
-				res.render('administration', { title: 'Administration', company: nconf.get('company').name,
-					customers: customerArr
-				});
+	knex.select().from('customer').then(function(rows) {
+			res.render('administration', { title: 'Tickets', company: nconf.get('company').name,
+				customers: rows
 			});
 	});
 });
