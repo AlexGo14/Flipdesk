4900462198557c1534ae0b217ea154ace3f97f06
Johannes Giere <jogiere@gmail.com>
2015-09-11 15:13:12 +0200

    Added license to files

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,3 +1,20 @@
+/*
+Flipdesk. A flexible helpdesk system.
+Copyright (C) 2015  Johannes Giere, jogiere AT gmail com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 function login_form() {
 	$.get('/authentication', {}, function(data) {
 		if(data.success) {
@@ -10,7 +27,7 @@ function login_form() {
 
 function login() {
 	$('#email_login')[0].value = $('#email_login')[0].value.toLowerCase();
-	
+
 	$.post('/authentication', $('#login_form').serialize(), function(data) {
 		if(data.success) {
 			if(data.login_pw_change) {
